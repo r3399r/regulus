fde646c84cb3eb6f53a7e6fba3e2f6329e60ea0b
Yue <yue@potentia.asia>
2023-09-19 10:20:45 +0800

    rerender process

--- frontend/src/page/print/index.tsx
+++ frontend/src/page/print/index.tsx
@@ -1,6 +1,6 @@
 import { Button, TextField } from '@mui/material';
 import { MathJax } from 'better-react-mathjax';
-import { useRef, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { useLocation } from 'react-router-dom';
 import { useReactToPrint } from 'react-to-print';
 import { GetQuestionResponse } from 'src/model/backend/api';
@@ -9,10 +9,16 @@ const Print = () => {
   const state = useLocation().state as { question: GetQuestionResponse } | null;
   const componentRef = useRef(null);
   const [width, setWidth] = useState<string>('8');
+  const [display, setDisplay] = useState<boolean>(true);
 
   const handlePrint = useReactToPrint({
     content: () => componentRef.current,
   });
+
+  useEffect(() => {
+    setDisplay(false);
+    setTimeout(() => setDisplay(true), 10);
+  }, [width]);
 
   return (
     <div>
@@ -28,29 +34,31 @@ const Print = () => {
           列印
         </Button>
       </div>
-      <div>
-        <MathJax dynamic>
-          <div className="mx-auto my-0 flex flex-wrap justify-center" ref={componentRef}>
-            {state?.question.map((v) => (
-              <div
-                key={v.id}
-                className="break-inside-avoid border border-solid border-black p-1"
-                style={{ width: `${width === '' ? 8 : width}cm` }}
-              >
-                <div className="text-sm text-gray-500">ID: {v.id.toUpperCase()}</div>
-                <div>{v.content}</div>
-                <div className="flex flex-wrap">
-                  {v.imageUrl?.map((o, i) => (
-                    <div key={i}>
-                      <img src={o} />
-                    </div>
-                  ))}
+      {display && (
+        <div ref={componentRef}>
+          <MathJax>
+            <div className="mx-auto my-0 flex flex-wrap justify-center">
+              {state?.question.map((v) => (
+                <div
+                  key={v.id}
+                  className="break-inside-avoid border border-solid border-black p-1"
+                  style={{ width: `${width === '' ? 8 : width}cm` }}
+                >
+                  <div className="text-sm text-gray-500">ID: {v.id.toUpperCase()}</div>
+                  <div>{v.content}</div>
+                  <div className="flex flex-wrap">
+                    {v.imageUrl?.map((o, i) => (
+                      <div key={i}>
+                        <img src={o} />
+                      </div>
+                    ))}
+                  </div>
                 </div>
-              </div>
-            ))}
-          </div>
-        </MathJax>
-      </div>
+              ))}
+            </div>
+          </MathJax>
+        </div>
+      )}
     </div>
   );
 };
