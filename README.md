4a3162dce0feb8fd27d475515fcef92979d94193
yue.chang <lamplighter.planet@gmail.com>
2024-05-28 01:54:47 +0800

    refactoring plan

--- README.md
+++ README.md
@@ -1,12 +1,5 @@
 # regulus
 online course
 
-# install dependencies
-docker-compose -f docker-compose.builder.yml run --rm install
-docker-compose -f docker-compose.builder.yml run --rm compile
-
-# dev mode
-docker-compse up
-
 # deploy test environment
 bash deploy.sh test
--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@ set -e
 
 env=$1
 project=regulus
-subDomain=yoho
+subDomain=regulus
 domain=celestialstudio.net
 
 echo ====================================================================================
@@ -12,31 +12,23 @@ echo project: $project
 echo domain: $subDomain.$domain
 echo ====================================================================================
 
-# echo execute db scripts...
-# cd ./db
-# host=$(aws ssm get-parameter --name $env-db-host | jq .Parameter.Value | sed -e 's/^"//' -e 's/"$//')
-# user=$(aws ssm get-parameter --name $env-db-user | jq .Parameter.Value | sed -e 's/^"//' -e 's/"$//')
-# pwd=$(aws ssm get-parameter --name $env-db-pwd | jq .Parameter.Value | sed -e 's/^"//' -e 's/"$//')
-# cluster=$(aws ssm get-parameter --name $env-db-cluster | jq .Parameter.Value | sed -e 's/^"//' -e 's/"$//')
-# psql postgresql://$user:$pwd@$host:26257/$cluster.$project -f deploy.sql
-# echo ====================================================================================
-
-echo install dependencies...
-docker-compose -f docker-compose.builder.yml run --rm install
-docker-compose -f docker-compose.builder.yml run --rm compile
-echo ====================================================================================
-
 echo deploy backend AWS...
-docker-compose -f docker-compose.builder.yml run --rm prepare-layers
-aws cloudformation deploy --template-file backend/packaged.yaml --stack-name $project-$env-stack --parameter-overrides TargetEnvr=$env Project=$project SubDomain=$subDomain Domain=$domain --no-fail-on-empty-changeset --s3-bucket y-cf-midway-singapore --capabilities CAPABILITY_NAMED_IAM
+cd backend
+npm i
+npm run clean
+npm install --production
+mkdir -p dist/nodejs
+cp -R node_modules dist/nodejs
+npm i
+npm run compile
+aws cloudformation package --template-file aws/cloudformation/template.yaml --output-template-file packaged.yaml --s3-bucket y-cf-midway-singapore
+aws cloudformation deploy --template-file packaged.yaml --stack-name $project-$env-stack --parameter-overrides TargetEnvr=$env Project=$project SubDomain=$subDomain Domain=$domain --no-fail-on-empty-changeset --s3-bucket y-cf-midway-singapore --capabilities CAPABILITY_NAMED_IAM
+cd ..
 echo ====================================================================================
 
-echo deploy frontend-console to S3...
-docker-compose -f docker-compose.builder.yml run --rm prepare-console
-aws s3 sync ./frontend-console/dist s3://$project-$env-console --delete --cache-control no-cache
-echo ====================================================================================
-
-echo deploy frontend-landing to S3...
-docker-compose -f docker-compose.builder.yml run --rm prepare-landing
-aws s3 sync ./frontend-landing/out s3://$project-$env-landing --delete --cache-control no-cache
+echo deploy frontend to S3...
+cd frontend
+npm i
+npm run pre:deploy
+aws s3 sync ./dist s3://$project-$env --delete --cache-control no-cache
 echo ====================================================================================
